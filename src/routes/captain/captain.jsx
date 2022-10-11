70654873a4b2c5b66982e2e8570724b6e54418b3
bhagavathi-HP <bhagavathi271@gmail.com>
2022-10-11 01:50:13 -0700

    captain commit

--- src/routes/captain/captain.jsx
+++ src/routes/captain/captain.jsx
@@ -6,17 +6,19 @@ const CaptainBox = () => {
   const captainAvailable = useContext(Available);
   console.log("Captain Available", captainAvailable);
   const [trig, setTrig] = useState(false);
+  const mytrig=() => setTrig(true);
   return (
     <div>
       <h1>Hi iam Captain page</h1>
 
-      <button className="borderbox 1" onClick={() => setTrig(true)}>
+      <button className="borderbox 1" onClick={mytrig}>
         Table 1
       </button>
       {trig && <Popup close={setTrig} />}
-      <button className="borderbox">Table 2</button>
-      <button className="borderbox">Table 3</button>
-      <button className="borderbox">Table 4</button>
+      <button className="borderbox" onClick={mytrig}>Table 2</button>
+      {trig && <Popup close={setTrig} />}
+      <button className="borderbox" onClick={mytrig}>Table 3</button>
+      <button className="borderbox" onClick={mytrig}>Table 4</button>
     </div>
   );
 };
